51150b1b63fb88f30690fc2a6a63d9f6da3316ad
Eric Schweitz <eschweitz@nvidia.com>
2026-06-04 13:52:03 -0700

    Fix syntax.
    Signed-off-by: Eric Schweitz <eschweitz@nvidia.com>

--- cudaq/test/AST-Quake/complex_literals.cpp
+++ cudaq/test/AST-Quake/complex_literals.cpp
@@ -6,7 +6,7 @@
  * the terms of the Apache License 2.0 which accompanies this distribution.    *
  ******************************************************************************/
 
-// UNSUPPORTED: *
+// UNSUPPORTED: always
 // RUN: cudaq-quake %s | FileCheck %s
 
 #include <cudaq.h>
